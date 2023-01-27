91e2ae18e9f75ce2dc34e4aedd24afbf382375ea
wizeKieu <kieunghiep@wize-net.co.jp>
2023-01-27 16:29:57 +0900

    Change route redirect while logout

--- app/Http/Controllers/Admin/AuthController.php
+++ app/Http/Controllers/Admin/AuthController.php
@@ -60,8 +60,7 @@ class AuthController extends Controller
     public function logout()
     {
         auth()->logout();
-
-        return response()->json(['message' => 'User successfully signed out']);
+        return redirect(route('admin.login'));
     }
 
     public function refresh()
