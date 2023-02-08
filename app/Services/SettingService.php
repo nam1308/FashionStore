51aa26e0bc0d7f6e99da8a77e45a4af5c69d8adc
wizeKieu <kieunghiep@wize-net.co.jp>
2023-02-08 18:53:52 +0900

    update setting

---
+++ app/Services/SettingService.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\ISettingRepository;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+
+class SettingService
+{
+    protected $interface;
+
+    public function __construct(ISettingRepository $interface)
+    {
+        $this->interface = $interface;
+    }
+
+    public function getSetting($params = [])
+    {
+        return $this->interface->getSetting($params);
+    }
+
+    public function save(Request $request)
+    {
+        try {
+            DB::beginTransaction();
+            $settings = $request->all();
+            foreach ($settings as $setting) {
+                Validator::validate($setting, [
+                    'value' => 'required',
+                    'key' => 'required'
+                ]);
+                $this->interface->save($setting);
+            }
+            DB::commit();
+            return true;
+        } catch (\Exception $exception) {
+            DB::rollBack();
+            throw new BadRequestHttpException($exception->getMessage());
+        }
+
+    }
+}
