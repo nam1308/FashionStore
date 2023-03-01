fc06de6ee8297b3ac02f7decba0e3e9e8d80c1b7
Quang Nguyen <nguyenminhquang2406@gmail.com>
2023-03-01 22:46:05 +0700

    fix: logic save option

--- resources/views/admin/product/edit.blade.php
+++ resources/views/admin/product/edit.blade.php
@@ -67,7 +67,7 @@
                                     </select>
                                 </div>
                             </div>
-                            <div class="col-4">
+                            <div v-if="isShow" class="col-4">
                                 <div class="form-group">
                                     <label>Regular Price</label>
                                     <input type="number" :value="products.regular_price" class="form-control" min="0">
@@ -113,6 +113,47 @@
                             <label>Meta Keyword</label>
                             <input v-model="products.meta_keyword" class="form-control">
                         </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="attribute">
+                <div class="ibox">
+                    <div class="ibox-head">
+                        <h3 class="ibox-title">Attribute product</h3>
+                    </div>
+                    <div class="ibox-body">
+                        <form class="form-horizontal">
+                            <div class="form-group row">
+                                <label class="col-sm-2 col-form-label">Attributes: </label>
+                                <div class="attribute-container col-sm-10">
+                                    <div v-for="(item,index) in options"
+                                         :key="index"
+                                         class="attributes__content mb-3 p-3 border border-secondary" >
+                                        <button
+                                            @click="removeAttribute($event,index)"
+                                            type="button" class="btn btn-sm btn-danger float-right mb-2">
+                                            <i class="ti-trash"></i>
+                                        </button>
+                                        <label>Attribute</label>
+                                        <select class="attribute-select form-control mb-2" :data-index="index">
+                                            <option value=""></option>
+                                            <option v-for='attr in item.attributes'
+                                            :selected="attr.selected"
+                                            :value='attr.slug'>@{{attr.name}}</option>
+                                        </select>
+                                        <label>Variation</label>
+                                        <select class="variation-select form-control" :data-index="index" multiple="multiple">
+                                            <option v-for='attr in item.variants'
+                                            :selected="attr.selected"
+                                            :data-parent="attr.parent" :value='attr.variant.name'>@{{attr.variant.name}}</option>
+                                        </select>
+                                    </div>
+                                    <button v-if="options.length < 3" type="button" @click="addAttribute" class="btn mr-2">Add attribute</button>
+                                    <button v-if="options.length" type="button" @click="saveOption" class="btn btn-primary">Save</button>
+                                </div>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
@@ -243,6 +284,9 @@
 
 @push('vue')
     <script type="text/javascript">
+        const table = new Map()
+        let indexAttribute = 0
+
         Vue.createApp({
             template: "#edit_product",
             data() {
@@ -269,9 +313,14 @@
                         attribute: [],
                     },
                     parents: [],
-                    dataTable: [],
+                    attributes: @json($attributes),// data attribute
+                    productAttribute: @json($productAttribute),
                     attrError: [],
-                    isShow: true,
+                    variants: [],
+                    options: [],//data option de render
+                    list: [],
+                    dataTable: [],
+                    isShow: true, //show price,
                     statusbtn: false,
                     bulkData: {price: 0, sku: null, stock: 0}
                 }
@@ -284,6 +333,36 @@
                 this.products = response.data[0];
                 this.dataTable = response.data[1];
                 PLUGIN.EDITOR(this.products.content);
+
+                const that = this;
+                $('.attribute-container').on('select2:select','.attribute-select',function name(e) {
+                    const slug = $(this).val()
+                    const index = $(this).data('index')
+                    that.setAttribute(e,$(this))
+                });
+                $('.attribute-container').on('select2:select','.variation-select',function name(e) {
+                    that.setVariant(e,$(this))
+                });
+                $('.attribute-container').on('select2:unselecting','.variation-select',function name(e) {
+                    const index = $(this).data('index')
+                    const parent = e.target.options[0].dataset.parent
+                    const value = $(this).val()
+                    that.options[index].variants.forEach((item,i) => {
+                        if (value.includes(item.variant.name)){
+                            that.options[index].variants[i].selected = false
+                        }
+                    })
+                });
+                $('.attribute-container').on('select2:unselect','.variation-select',function name(e) {
+                    const index = $(this).data('index')
+                    const parent = e.target.options[0].dataset.parent
+                    const value = $(this).val()
+                    that.list[index].option = {
+                        [parent]: value
+                    }
+                });
+
+                this.initOption();
             },
             methods: {
                 generateSlug(e){
@@ -300,6 +379,213 @@
                     } catch (e) {
                         this.products.error = e.response.data;
                     }
+                },
+
+                saveOption(e){
+                    const length = this.list.length
+                    let n = 0
+                    let m = 0
+                    const res = []
+
+                    this.list.forEach(item => {
+                        if (!Object.keys(item.option).length){
+                            alert('Attribute not empty')
+                            throw new Error('Attribute not empty');
+                        }
+                    })
+
+                    for (const key in this.list[0].option) {
+                        const item = this.list[0]
+                        const tmp = item.option[key].map(item => {
+                            return length == 1 ?
+                            {
+                                option: {
+                                    [key]: item
+                                },
+                                price: 0,
+                                sku: '',
+                                stock: 0,
+                                thumb_url: '/medias/1675648139.jpg',
+                            }
+                            :
+                            {
+                                [key]: item,
+                            }
+                        })
+                        res.push(tmp)
+                    }
+
+                    for (let i = 1; i < length; i++) {
+                        const elememt = this.list[i].option;
+                        const prev = res[i-1]
+                        let item = []
+                        let tmp = []
+
+                        for (const key in elememt) {
+                            item = elememt[key].map(o => {
+                                return{
+                                    [key]: o
+                                }
+                            })
+                        }
+
+
+                        for (let j = 0; j < prev.length; j++) {
+                            for (let k = 0; k < item.length; k++) {
+                                // console.log(prev[j]);
+                                tmp.push({
+                                    option: i == 1 ? {
+                                        ...prev[j],
+                                        ...item[k]
+                                    } : {
+                                        ...prev[j].option,
+                                        ...item[k]
+                                    },
+                                    price: 0,
+                                    sku: '',
+                                    stock: 0,
+                                    thumb_url: '/medias/1675648139.jpg',
+                                })
+                            }
+                        }
+                        res.push(tmp)
+                    }
+
+                    this.dataTable = res[res.length-1]
+                    console.log(this.dataTable);
+                },
+
+                initOption(){
+                    let i = 0
+
+                    for (const key in this.productAttribute){
+                        const obj = this.productAttribute[key];
+                        this.addAttribute()
+                        this.$nextTick(() => {
+                            const attr = $(`.attribute-select[data-index=${i}]`)
+                            attr.val(key).trigger('change')
+                            this.setAttribute(null,attr,obj)
+                            i++;
+                        })
+                    }
+
+                },
+
+                setAttribute(e,elem,data){
+                    const value = elem ? elem.val() : e.target.value;
+                    const index = elem ? elem.data('index') :e.target.dataset.index;
+                    const attribute =  this.attributes.find((item) => item.slug === value);
+                    const variants = attribute?.children ?? []
+
+                    if (table.has(value) && table.get(value) !== index){
+                        const indexDup = table.get(value)
+
+                        this.options[indexDup].attributes.forEach((item,i) => {
+                            if(item.slug === value)
+                                this.options[indexDup].attributes[i].selected = false
+                        })
+
+                        $(`.attribute-select[data-index=${indexDup}]`).val(null).trigger('change')
+
+                        this.options[indexDup].variants = []
+
+                        PLUGIN.INIT(`.variation-select[data-index=${indexDup}]`,{
+                            placeholder: 'Variation',
+                            allowClear: true
+                        })
+
+                        this.list[indexDup] = {
+                            option: {},
+                        }
+
+                    }
+
+                    table.set(value,index)
+
+                    this.options[index].attributes.forEach((item,i) => {
+                        if(item.slug === value)
+                        this.options[index].attributes[i].selected = true
+                    })
+
+                    this.options[index].variants = variants.map(item => {
+                        return {
+                            parent: value,
+                            variant: item,
+                            selected: false
+                        }
+                    })
+                    this.list[index] = {
+                        option: {...this.list[index]?.option ?? [], [attribute.slug]: []},
+                    }
+                    this.$nextTick(() => {
+                        PLUGIN.INIT('.variation-select',{
+                            placeholder: 'Variation',
+                            allowClear: true
+                        })
+
+                        if (data){
+                            const opt = $(`.variation-select[data-index=${index}]`)
+                            opt.val(data).trigger('change')
+                            this.setVariant(null,opt)
+                        }
+                    })
+
+                },
+
+                setVariant(e,elem){
+                    const value = elem.val();
+                    const parent = elem.find('option:first-child').data('parent');
+                    const index = elem.data('index');
+                    const dataIndex = this.list[index];
+                    this.options[index].variants.forEach((item,i) => {
+                        if(value.includes(item.variant.name)){
+                            this.options[index].variants[i].selected = true
+                        }
+                    })
+
+                    this.list[index] = {
+                        ...dataIndex,
+                        option: {...dataIndex.option, [parent]: [...value]}
+                    }
+
+                    console.log(this.list);
+                },
+
+                addAttribute(){
+                    this.isShow = false;
+                    const attribute = this.attributes.map(item => {
+                        return {
+                            ...item,
+                            selected: false
+                        }
+                    })
+                    this.options = [...this.options,{attributes: attribute,variants: []}]
+
+                    this.list.push({})
+
+                    this.$nextTick(() => {
+                        PLUGIN.INIT('.attribute-select',{
+                            placeholder: 'Attribute',
+                            allowClear: true
+                        })
+                    })
+                },
+
+                removeAttribute(e,index){
+                    table.clear()
+                    this.options.splice(index,1)
+                    this.list.splice(index,1)
+                    if (this.options.length == 0){
+                        this.isShow = true
+                    }
+
+                    this.$nextTick(() => {
+                        $('.attribute-select').trigger('change')
+                        PLUGIN.INIT('.variation-select',{
+                            placeholder: 'Variation',
+                            allowClear: true
+                        })
+                    })
                 },
 
                 errorIf(index, key) {
